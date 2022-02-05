ca5dca11ab6ed996dd476b85a98d9cd0d3d53e4d
Chris Young <ChrisC.Young1@gmail.com>
2022-02-05 11:22:32 -0800

    imported models into models.__init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,6 @@
 from .db import db
 from .user import User
+from app.models.project import Project
+from app.models.instruction import Instruction
+from app.models.supply import Supply
+from app.models.comment import Commnet
