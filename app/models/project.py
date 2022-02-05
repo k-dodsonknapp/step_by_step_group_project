16eedda0ba4e86d33de8cbe719bae6200c93d242
Chris Young <ChrisC.Young1@gmail.com>
2022-02-05 10:52:41 -0800

    project model built

---
+++ app/models/project.py
@@ -0,0 +1,13 @@
+from .db import db
+
+class Project(db.Model):
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    titleImage = db.Column(db.Text, nullable=False)
+    overview = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+
+    user = db.relationship('User', back_populates='project')
