c2f48c15f8739359e85f91b3a3a6227305faa513
Chris Young <ChrisC.Young1@gmail.com>
2022-02-07 09:52:23 -0800

    routes

--- app/api/project_routes.py
+++ app/api/project_routes.py
@@ -5,7 +5,18 @@ project_routes = Blueprint('project', __name__)
 
 
 @project_routes.route('/')
-
-def project():
+def projects():
     projects = Project.query.all()
-    
+    return {'projects': [project.to_dict() for project in projects]}
+
+
+@project_routes.route('/<int:id>')
+def project(id):
+    project = Project.query.get(id)
+    return project.to_dict()
+
+
+@project_routes.route('/:category')
+def projects_by_category(category):
+    projects = Project.query.filter(Project.category == category)
+    return {'projects': [project.to_dict() for project in projects]}
