be4263362c8e7d0526cbd996a3e7f4d6cc57c322
anthony <awadams198@gmail.com>
2022-02-07 16:30:35 -0500

    added project and comment reducer and also added components

---
+++ react-app/src/store/comments.js
@@ -0,0 +1,106 @@
+const GET_COMMENTS = "/comments/";
+const ADD_COMMENT = "/comments/new";
+const UPDATE_COMMENT = "/comments/update";
+const DELETE_COMMENT = "/comments/delete";
+
+const getComments = (comments) => ({
+  type: GET_COMMENTS,
+  payload: comments,
+});
+
+const addComment = (comment) => ({
+  type: ADD_COMMENT,
+  payload: comment,
+});
+
+const updateComment = (comment) => ({
+  type: UPDATE_COMMENT,
+  payload: comment,
+});
+
+const deleteComment = (comment) => ({
+  type: DELETE_COMMENT,
+  payload: comment,
+});
+
+export const getAllComments = (projectId) => async (dispatch) => {
+  const response = await fetch(`/api/projects/${projectId}/comments`);
+  if (response.ok) {
+    const data = await response.json();
+    if (data.errors) {
+      return;
+    }
+
+    dispatch(getComments(data));
+    return data;
+  }
+};
+
+export const addOneComment = (comment) => async (dispatch) => {
+  const response = await fetch(`/api/projects/${comment.projectId}/comments`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(comment),
+  });
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(addComment(data));
+    return data;
+  }
+};
+
+export const updateOneComment = (comment) => async (dispatch) => {
+  const response = await fetch(`/api/comments/${comment.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(comment),
+  });
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(updateComment(data));
+    return data;
+  }
+};
+export const deleteOneComment = (id) => async (dispatch) => {
+  const res = await fetch(`/api/comments/${id}`, {
+    method: "DELETE",
+  });
+  if (res.ok) {
+    dispatch(deleteComment(id));
+    return "Successfully deleted.";
+  }
+};
+
+const initialState = {};
+
+export default function commentReducer(state = initialState, action) {
+  let newState;
+  switch (action.type) {
+    case GET_COMMENTS:
+      newState = {};
+      action.payload.comments.map(
+        (comment) => (newState[comment.id] = comment)
+      );
+      return newState;
+    case ADD_COMMENT:
+      newState = {
+        ...state,
+        [action.payload.id]: action.payload,
+      };
+      return newState;
+    case UPDATE_COMMENT:
+      state[action.payload.comment.id] = action.payload.comment;
+      newState = { ...state };
+      return newState;
+    case DELETE_COMMENT:
+      newState = { ...state };
+      delete newState[action.payload];
+      return newState;
+    default:
+      return state;
+  }
+}
---
+++ react-app/src/store/project.js
@@ -0,0 +1,107 @@
+const GET_PROJECTS = "/projects/";
+const ADD_PROJECTS = "/projects/new";
+const UPDATE_PROJECTS = "/projects/update";
+const DELETE_PROJECTS = "/projects/delete";
+
+const getProjects = (project) => ({
+  type: GET_PROJECTS,
+  payload: project,
+});
+
+const addProjects = (projects) => ({
+  type: ADD_PROJECTS,
+  payload: projects,
+});
+
+const updateProjects = (projects) => ({
+  type: UPDATE_PROJECTS,
+  payload: projects,
+});
+
+const deleteProjects = (projectId) => ({
+  type: DELETE_PROJECTS,
+  payload: projectId,
+});
+
+export const getAllProjects = () => async (dispatch) => {
+  const response = await fetch("/api/projects");
+  if (response.ok) {
+    const data = await response.json();
+    if (data.errors) {
+      return;
+    }
+    dispatch(getProjects(data));
+    return data;
+  }
+};
+export const addOneProject = (data) => async (dispatch) => {
+  const response = await fetch("/api/projects/new", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(addProjects(data));
+    return data;
+  }
+};
+
+export const updateOnePost = (data) => async (dispatch) => {
+  const response = await fetch(`/api/projects/${data.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(updateProjects(data));
+    return data;
+  }
+};
+
+export const deleteOneProject = (projectId) => async (dispatch) => {
+  const response = await fetch(`/api/projects/${projectId}`, {
+    method: "DELETE",
+  });
+  if (response.ok) {
+    dispatch(deleteProjects(projectId));
+    return "Deleted.";
+  }
+};
+
+const initialState = {};
+
+export default function projectReducer (state = initialState, action) {
+  let newState;
+  switch (action.type) {
+    case GET_PROJECTS:
+      newState = { ...state };
+      action.payload.projects.map((project) => newState[project.id] = project);
+      return newState;
+
+    case ADD_PROJECTS:
+      newState = {
+        ...state,
+        [action.payload.id]: action.payload,
+      };
+      return newState;
+
+    case UPDATE_PROJECTS:
+      state[action.payload.id] = action.payload;
+      newState = { ...state };
+      return newState;
+
+    case DELETE_PROJECTS:
+      newState = { ...state };
+      delete newState[action.payload];
+      return newState;
+
+    default:
+      return state;
+  }
+}
