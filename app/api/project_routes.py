13e4c674d0004a738795453787cd88ed1c3e31e9
Chris Young <ChrisC.Young1@gmail.com>
2022-02-07 13:38:30 -0800

    backend project route tested and working

--- app/api/project_routes.py
+++ app/api/project_routes.py
@@ -14,23 +14,22 @@ def projects():
 @project_routes.route('/<int:id>')
 def project(id):
     project = Project.query.get(id).to_dict()
-    # project = project.to_dict()
-    owner = User.query.filter(User.id == project.userId).first()
+    owner = User.query.filter(User.id == project['userId']).first()
     supplies = Supply.query.filter(Supply.projectId == id).all()
     instructions = Instruction.query.filter(Instruction.projectId == id).all()
     comments = Comment.query.filter(Comment.projectId == id).all()
 
     return {'project': {
-        'title': project.title,
+        'title': project['title'],
         'owner': owner.to_dict(),
-        'overview': project.overview,
+        'overview': project['overview'],
         'supplies': [supply.to_dict() for supply in supplies],
         'instructions': [instruction.to_dict() for instruction in instructions],
         'comments': [comment.to_dict() for comment in comments]
     }}
 
 
-@project_routes.route('/:category')
+@project_routes.route('/<category>')
 def projects_by_category(category):
     projects = Project.query.filter(Project.category == category)
     return {'projects': [project.to_dict() for project in projects]}
