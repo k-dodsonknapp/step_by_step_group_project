a59033ed75f97f8db8e952051f689e9428c2cce5
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-02-07 16:36:03 -0800

    added stepTitle column to instructions table

--- app/seeds/instruction_seeder.py
+++ app/seeds/instruction_seeder.py
@@ -5,6 +5,7 @@ def seed_instructions():
     bird_project1= Instruction(
         projectId=1,
         stepOrder=1,
+        stepTitle="Get materials",
         instructions="Gather your materials", 
         photoUrl="https://imgprd19.hobbylobby.com/2/0e/4f/20e4f0a77628cacbbbb622fb4a01f7728b821d88/350Wx350H-116392-1019-px.jpg", 
         videoUrl="Video of materials"
@@ -13,6 +14,7 @@ def seed_instructions():
     bird_project2= Instruction(
         projectId=1,
         stepOrder=2,
+        stepTitle="Start square",
         instructions="Take two pieces of wood and use wood glue put the two together", 
         photoUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbYADPGL1BShmV9Nw9K12GCVu5SAR1pZ_0cBKV9AYyaakIoj-rZMdQF2cDIkWaM15sIGA&usqp=CAU", 
         videoUrl="Video of the action"
@@ -21,6 +23,7 @@ def seed_instructions():
     bird_project3= Instruction(
         projectId=1,
         stepOrder=3,
+        stepTitle="Add rest of square",
         instructions="Repeat step 2 with the last two pieces of the square", 
         photoUrl="Picture of wood pieces and glue", 
         videoUrl="Video of the action"
@@ -29,6 +32,7 @@ def seed_instructions():
     bird_project4= Instruction(
         projectId=1,
         stepOrder=4,
+        stepTitle="Add Roof",
         instructions="Add roof", 
         photoUrl="Picture of wood pieces and glue", 
         videoUrl="Video of the action"
@@ -37,9 +41,16 @@ def seed_instructions():
     bird_project5= Instruction(
         projectId=1,
         stepOrder=5,
+        stepTitle="Add Shingles",
         instructions="Add shingles to roof", 
         photoUrl="Picture of wood pieces and glue", 
         videoUrl="Video of the action"
+    )
+
+    casino_clock1=Instruction(
+        projectId=2,
+        stepOrder=1,
+        instructions=""
     )
     
     db.session.add(bird_project1)
--- app/seeds/projects_seeder.py
+++ app/seeds/projects_seeder.py
@@ -11,7 +11,30 @@ def seed_project():
         category="Crafts"
     )
 
+    casino_clock= Project(
+        userId=2, 
+        title="Casino'clock - Playing card flap display / clock", 
+        titleImage="https://content.instructables.com/ORIG/F8U/KR3T/KZCPHOFJ/F8UKR3TKZCPHOFJ.jpg?auto=webp&frame=1&width=1024&height=1024&fit=bounds&md=16727930c4e9a859906b057dd58a338d", 
+        overview="""
+            One of the main difficulty to make a split flap display with 3D printer is the fabrication of flaps. 
+            Therefore I used off-the-shelf playing cards for flaps, and made a funny and crazy clock with minimum number of parts. 
+            Simple design : each display unit consists of only 3 types of parts. Sensor is not used to find the origin. 
+            Funny motion : by arranging cards randomly, you can enjoy busy motion of flaps 
+            WiFi time acquisition : current time is acquired via WiFi using ntp
+            """,
+        category="Circuits"
+    )
+
+    # bird_house= Project(
+    #     userId=1, 
+    #     title="How to build a bird house.", 
+    #     titleImage="idk", 
+    #     overview="This is how to construct a bird house!",
+    #     category="Crafts"
+    # )
+
     db.session.add(bird_house)
+    db.session.add(casino_clock)
     db.session.commit()
 
 def undo_project():
