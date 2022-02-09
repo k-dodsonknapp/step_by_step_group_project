176f2df9c3a81ced79c52d49ff49d208e4453fbf
Chris Young <ChrisC.Young1@gmail.com>
2022-02-09 08:57:52 -0800

    footer started

---
+++ react-app/src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            <ul class='categories-list'>Categories
+                <li>Circuits</li>
+                <li>Workshop</li>
+                <li>Craft</li>
+                <li>Cooking</li>
+                <li>Living</li>
+                <li>Outside</li>
+                <li>Teachers</li>
+            </ul>
+        </footer>
+    )
+
+}
+
+export default Footer
