3d2ea89937f1cde0f9359d391d8a2a4df5fd0ccb
Chris Young <ChrisC.Young1@gmail.com>
2022-02-10 10:19:07 -0800

    steps added

--- react-app/src/components/ProjectDetails/index.js
+++ react-app/src/components/ProjectDetails/index.js
@@ -18,27 +18,44 @@ const ProjectDetails = () => {
     return (
         <>
             {project &&
-                <>
+                <div id='project-container'>
                     {/* <h1>Hello from Project by id page</h1> */}
-                    <div class='title'>{project.title}</div>
+                    <div className='title'>{project.title}</div>
                     <div id='project-details'>By
                         <span className='username-category'>{project.owner.username}</span>
                         in<span className='username-category'>{project.category}</span>
                     </div>
                     <div className="project-image-container">
-                        <img classname='project-images'src={project.titleImage} alt='Completed project'></img>
+                        <img className='project-images'src={project.titleImage} alt='Completed project'></img>
                     </div>
-                    <div id='overview-container'>
-                        <div id='overview-title'>Project Overview:
-                            <p id='project-overview'>{project.overview}</p>
-                        </div>
+                    <div id='overview-title'>Project Overview:
+                        <p id='project-overview'>{project.overview}</p>
                     </div>
+                    <ul id='supplies-title'>Supplies Needed:
+                        {project.supplies.map((supply) => (
+                            <>
+                                <li className='supply-list'key={supply.id}>{supply.supply}</li>
+                            </>
+                        ))}
+
+                    </ul>
                     <ul>
                         {project.instructions.map((instruction) => (
-                            <li key={instruction.id}>{instruction.instructions}</li>
+                            <div className="instruction-container">
+                                <step className='instruction-title'>Step {instruction.stepOrder}:</step>
+                                <div className='project-image-container'>
+                                    <img
+                                        className="instruction-image"
+                                        key={instruction.id}
+                                        src={instruction.photoUrl}
+                                        alt={`Step ${instruction.stepOrder}`}>
+                                    </img>
+                                </div>
+                                <li className='instructions'key={instruction.id}>{instruction.instructions}</li>
+                            </div>
                         ))}
                     </ul>
-                </>
+                </div>
             }
         </>
     )
