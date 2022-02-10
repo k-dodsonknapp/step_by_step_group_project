d20892784944adafb422fe92a50fa6d8a99ac0c4
Chris Young <ChrisC.Young1@gmail.com>
2022-02-10 09:31:34 -0800

    getting overview done

--- react-app/src/components/ProjectDetails/index.js
+++ react-app/src/components/ProjectDetails/index.js
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom"
 import { getOneProject } from "../../store/project";
+import './Projects.css'
 
 const ProjectDetails = () => {
     const dispatch = useDispatch()
@@ -12,15 +13,26 @@ const ProjectDetails = () => {
 
     useEffect(() => {
         dispatch(getOneProject(projectId))
-    }, [])
+    }, [dispatch, projectId])
 
     return (
         <>
             {project &&
                 <>
-                    <h1>Hello from Project by id page</h1>
+                    {/* <h1>Hello from Project by id page</h1> */}
                     <div class='title'>{project.title}</div>
-                    <div>{project.owner.username}</div>
+                    <div id='project-details'>By
+                        <span className='username-category'>{project.owner.username}</span>
+                        in<span className='username-category'>{project.category}</span>
+                    </div>
+                    <div className="project-image-container">
+                        <img classname='project-images'src={project.titleImage} alt='Completed project'></img>
+                    </div>
+                    <div id='overview-container'>
+                        <div id='overview-title'>Project Overview:
+                            <p id='project-overview'>{project.overview}</p>
+                        </div>
+                    </div>
                     <ul>
                         {project.instructions.map((instruction) => (
                             <li key={instruction.id}>{instruction.instructions}</li>
