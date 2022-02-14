774684bd8e47ae3e75d47044a23b3523f4d19879
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-02-13 23:17:14 -0800

    CRUD WORKS FOR FEATURES

--- react-app/src/components/EditComments/index.js
+++ react-app/src/components/EditComments/index.js
@@ -9,17 +9,21 @@ import {
   deleteOneComment,
 } from "../../store/comments";
 
-const EditCommentForm = () => {
-  const { commentId } = useParams();
+const EditCommentForm = ({ commentId, projectId }) => {
+  // const { commentId } = useParams();
+  // console.log(commentId)
+  const history = useHistory();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(updateOneComment(commentId));
   }, [dispatch, commentId]);
 
-  const comment = useSelector((state) => state.comment[commentId]);
+  const comment = useSelector((state) => state.comments[commentId]);
+  console.log("HOPEFULLY", comment)
   const sessionUser = useSelector((state) => state.session.user);
 
+  const [idPath, setIdPath] = useState("")
   const [name, setName] = useState(comment?.id);
   const [body, setBody] = useState(comment?.comment);
   const updateBody = (e) => setBody(e.target.value);
@@ -31,27 +35,38 @@ const EditCommentForm = () => {
     e.preventDefault();
 
     const payload = {
-      ...comment,
+      "comment": body,
+      commentId
     };
 
-    await dispatch(updateOneComment(payload));
+    const data = await dispatch(updateOneComment(payload));
+    console.log("ERIC SMELLS", data.projectId)
+    setIdPath(data.id)
+    // history.push("/")
+    // history.push(`/projects/${data.projectId}`)
+    window.location.reload(true)
 
-    if (sessionUser) {
-      return (
-        <>
-          <form onSubmit={handleSubmit}>
-            <input type="text" value={name} onChange={updateBody} required />
-            <textarea value={body} onChange={updateBody} required />
-            <button type="submit">Edit</button>
+  }
 
-            <button className="options" id="del-button" onClick={ahandleSubmit}>
-              Delete
-            </button>
-          </form>
-        </>
-      );
-    }
-  };
+  useEffect(() => {
+    dispatch(getOneProject(projectId))
+  }, [dispatch, idPath, body])
+
+  // if (sessionUser) {
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={body} onChange={e => setBody(e.target.value)} required />
+        {/* <textarea value={body} onChange={updateBody} required /> */}
+        <button type="submit">Edit</button>
+
+        {/* <button className="options" id="del-button" onClick={handleSubmit}> */}
+        {/* Delete */}
+        {/* </button> */}
+      </form>
+    </>
+  );
+  // };
 };
 
 export default EditCommentForm;
