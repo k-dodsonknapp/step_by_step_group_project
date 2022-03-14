dc52fa85ac15ba135177439d0e058e4a72d3b3b6
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-03-14 10:36:03 -0700

    fixed warning for EditComments.js

--- react-app/src/components/EditComments/index.js
+++ react-app/src/components/EditComments/index.js
@@ -1,18 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { getOneProject } from "../../store/project";
-import {
-  getAllComments,
-  addOneComment,
-  updateOneComment,
-  deleteOneComment,
-} from "../../store/comments";
+import { updateOneComment, deleteOneComment } from "../../store/comments";
 
-const EditCommentForm = ({ commentId, projectId, commentUserId }) => {
-  // const { commentId } = useParams();
-  // console.log(commentId)
-  const history = useHistory();
+const EditCommentForm = ({ commentId, projectId }) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -20,41 +12,26 @@ const EditCommentForm = ({ commentId, projectId, commentUserId }) => {
   }, [dispatch, commentId]);
 
   const comment = useSelector((state) => state.comments[commentId]);
-  console.log("HOPEFULLY", comment)
-  // const sessionUser = useSelector((state) => state.session.user);
-  const user = useSelector((state) => state.session.user);
 
   const [idPath, setIdPath] = useState("")
-  const [name, setName] = useState(comment?.id);
   const [body, setBody] = useState(comment?.comment);
   const [editClicked, setEditClicked] = useState(true)
-  const updateBody = (e) => setBody(e.target.value);
-  const ahandleSubmit = async (e) => {
-    e.preventDefault();
-    dispatch(deleteOneComment(commentId));
-  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const payload = {
       "comment": body,
       commentId
     };
-
     const data = await dispatch(updateOneComment(payload));
-    // console.log("ERIC SMELLS", data.projectId)
     setEditClicked(false)
     setIdPath(data.id)
-    // history.push("/")
-    // history.push(`/projects/${data.projectId}`)
-    // window.location.reload(true)
-
   }
 
   useEffect(() => {
     dispatch(getOneProject(projectId))
     dispatch(updateOneComment(commentId))
-  }, [dispatch, idPath, body])
+  }, [dispatch, idPath, body, commentId, projectId])
 
   // if (sessionUser) {
   return (
@@ -65,13 +42,13 @@ const EditCommentForm = ({ commentId, projectId, commentUserId }) => {
         <input type="text" value={body} onChange={e => setBody(e.target.value)} required />
         {/* <textarea value={body} onChange={updateBody} required /> */}
         <button className="submit-comment" type="submit">Edit Comment</button>
-        
+
 
         {/* <button className="options" id="del-button" onClick={handleSubmit}> */}
         {/* Delete */}
         {/* </button> */}
       </form>
-        )}
+    )}
     </>
   );
   // };
