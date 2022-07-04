c7f76cf3f7f1293c67d06781e22edb5a9c058007
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-07-04 13:36:45 -0700

    Added favorite routes

--- app/api/favorite.py
+++ app/api/favorite.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, Request, request
 from app.models import db
 from app.models.favorite import Favorite
 
@@ -15,3 +15,31 @@ def get_one_projects_favorites(id):
     favorites = Favorite.query.filter(Favorite.projectId == id).all()
     return {"favorite": [favorite.to_dict() for favorite in favorites]}
     
+
+@favorite_routes.route('/add', methods=["POST"])
+def add_favorite():
+    data = request.json
+
+    userId = data["userId"]
+    projectId = data["projectId"]
+
+    new_favorite = Favorite(
+        userId = userId,
+        projectId = projectId
+    )
+
+    db.session.add(new_favorite)
+    db.session.commit()
+
+    return new_favorite.to_dict()
+
+@favorite_routes.route("/delete", methods=["DELETE"])
+def delete_favorite():
+    data = request.json
+    id = data["id"]
+
+    favorite = Favorite.query.get(id)
+    if favorite:
+        db.session.delete(favorite)
+        db.session.commit()
+    return {"message": "Delete Successful"}
--- app/seeds/favorite.py
+++ app/seeds/favorite.py
@@ -6,7 +6,13 @@ def seed_favorites():
         userId=1,
     )
 
+    project_favorite2 = Favorite(
+        projectId=1,
+        userId=2,
+    )
+
     db.session.add(project1_favorite)
+    db.session.add(project_favorite2)
 
     db.session.commit()
 
