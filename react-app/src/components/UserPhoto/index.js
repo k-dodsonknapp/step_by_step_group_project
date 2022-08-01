38ffe071d7097d1ca29a97055d8f7ed7503ee7b2
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-08-01 06:29:37 -0700

    cleaning up code 3

--- react-app/src/components/UserPhoto/index.js
+++ react-app/src/components/UserPhoto/index.js
@@ -4,22 +4,25 @@ function UserPhoto({userId}) {
   console.log(userId)
   const [userPhoto, setUserPhoto] = useState(null)
   console.log(userPhoto)
-  useEffect(async() => {
-    const response = await fetch(`/api/users/${userId}`);
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data)
-      if (data.errors) {
-        return;
-      };
-      // dispatch(getProjects(data));
-      // return data;
-      setUserPhoto(data.userPhoto)
+  useEffect(() => {
+    async function fetchData(){
+      const response = await fetch(`/api/users/${userId}`);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data)
+        if (data.errors) {
+          return;
+        };
+        // dispatch(getProjects(data));
+        // return data;
+        setUserPhoto(data.userPhoto)
+      }
     };
+    fetchData()
   }, [userId])
   return (
     <>
-    <img id='comment-user-photo' src={userPhoto}></img>
+    <img id='comment-user-photo' src={userPhoto} alt={""}></img>
     </>
   )
 }
