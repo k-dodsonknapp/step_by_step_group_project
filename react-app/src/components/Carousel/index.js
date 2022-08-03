22eedaf99952cfd653dcad244e3ba29d047f7c0c
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-08-02 20:19:20 -0700

    added carousel auto scroll

--- react-app/src/components/Carousel/index.js
+++ react-app/src/components/Carousel/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Carousel.css"
 
 export const CarouselItem = ({ children, width }) => {
@@ -13,13 +13,62 @@ function Carousel({ children }) {
 
     const [activeIndex, seActiveIndex] = useState(0);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            updateIndex(activeIndex + 1)
+        }, 5000)
+        return () => {
+            if (interval) {
+                clearInterval(interval)
+            }
+        }
+    })
+
+    const updateIndex = (newIndex) => {
+        if (newIndex < 0) newIndex = React.Children.count(children) - 1;
+        else if (newIndex >= React.Children.count(children)) {
+            newIndex = 0;
+        }
+
+        seActiveIndex(newIndex)
+    }
+
     return (
         <div className='carousel'>
+                    {/* <button
+                        onClick={() => {
+                            updateIndex(activeIndex - 1);
+                        }}>
+                        Prev
+                    </button> */}
             <div className='inner' style={{ transform: `translateX(-${activeIndex * 100}%)` }}>
-                {React.Children.map(children, (child, index) => {
-                    return React.cloneElement(child, { width: "100%" });
-                })}
+                <div>
+
+                    {React.Children.map(children, (child, index) => {
+                        return React.cloneElement(child, { width: "100%" });
+                    })}
+                </div>
             </div>
+                    {/* <button
+                        onClick={() => {
+                            updateIndex(activeIndex + 1);
+                        }}>
+                        Next
+                    </button> */}
+            {/* <div className='indicators'>
+                <button
+                    onClick={() => {
+                        updateIndex(activeIndex - 1);
+                    }}>
+                    Prev
+                </button>
+                <button
+                    onClick={() => {
+                        updateIndex(activeIndex + 1);
+                    }}>
+                    Next
+                </button>
+            </div> */}
         </div>
     )
 }
