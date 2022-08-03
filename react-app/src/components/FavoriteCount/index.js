f37b3409b84d41b93444ac596b9a3afd49e67654
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-08-03 09:01:58 -0700

    start error

--- react-app/src/components/FavoriteCount/index.js
+++ react-app/src/components/FavoriteCount/index.js
@@ -5,13 +5,23 @@ import { getAllFavorites } from '../../store/favortie';
 function FavoriteCount({ project }) {
 
     const dispatch = useDispatch();
-    const favorites = useSelector(state => state?.favorites?.favorite).filter(favorite => favorite?.projectId === project?.id)
-    const [favoriteCount, setFavoriteCount] = useState(0)
-
+    const favorites = useSelector(state => state?.favorites?.favorite)
+    const [favoriteCount, setFavoriteCount] = useState(0);
+    const [favoriteArr, setFavoriteArr] = useState([]);
+    console.log("ASDFASDFASDF",favoriteArr)
+    
     useEffect(() => {
         dispatch(getAllFavorites())
+        const favoriteList = favorites.filter(favorite => favorite?.projectId === project?.id)
+        if (favoriteList){
+            setFavoriteArr(favoriteList)
+        }
         if (favorites?.length){
-            setFavoriteCount(favorites?.length)
+            setFavoriteCount(favoriteList?.length)
+            console.log("yuio",favoriteCount)
+        }else {
+            setFavoriteCount(0)
+            console.log("yuio",favoriteCount)
         }
     }, [dispatch])
 
