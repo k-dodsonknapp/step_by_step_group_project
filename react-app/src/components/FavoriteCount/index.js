8cdc6dedd5315b9d31e8d106714b25115d37eb6a
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-08-03 09:11:43 -0700

    start error

--- react-app/src/components/FavoriteCount/index.js
+++ react-app/src/components/FavoriteCount/index.js
@@ -5,29 +5,31 @@ import { getAllFavorites } from '../../store/favortie';
 function FavoriteCount({ project }) {
 
     const dispatch = useDispatch();
-    const favorites = useSelector(state => state?.favorites?.favorite)
-    const [favoriteCount, setFavoriteCount] = useState(0);
-    const [favoriteArr, setFavoriteArr] = useState([]);
-    console.log("ASDFASDFASDF",favoriteArr)
+    const favorites = useSelector(state => state?.favorites?.favorite)?.filter(favorite => favorite?.projectId === project?.id)
+    // const [favoriteCount, setFavoriteCount] = useState(0);
+    // const [favoriteArr, setFavoriteArr] = useState([]);
+    // console.log("ASDFASDFASDF",favoriteArr)
     
     useEffect(() => {
         dispatch(getAllFavorites())
-        const favoriteList = favorites.filter(favorite => favorite?.projectId === project?.id)
-        if (favoriteList){
-            setFavoriteArr(favoriteList)
-        }
-        if (favorites?.length){
-            setFavoriteCount(favoriteList?.length)
-            console.log("yuio",favoriteCount)
-        }else {
-            setFavoriteCount(0)
-            console.log("yuio",favoriteCount)
-        }
+        // const favoriteList = favorites?.filter(favorite => favorite?.projectId === project?.id)
+        // if (favoriteList !== undefined){
+        //     setFavoriteCount(favoriteList)
+        // } else {
+        //     setFavoriteArr(0)
+        // }
+        // if (favorites?.length){
+        //     setFavoriteCount(favoriteList?.length)
+        //     console.log("yuio",favoriteCount)
+        // }else {
+        //     setFavoriteCount(0)
+        //     console.log("yuio",favoriteCount)
+        // }
     }, [dispatch])
 
     return (
         <div id='favorite-count'>
-            ❤ {favoriteCount}
+            ❤ {favorites?.length}
         </div>
     )
 }
