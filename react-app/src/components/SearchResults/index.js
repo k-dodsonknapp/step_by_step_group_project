cec2abf2eb64cccfe877cbe3d3d1755cf3a850f4
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-08-03 07:50:43 -0700

    styled search input

--- react-app/src/components/SearchResults/index.js
+++ react-app/src/components/SearchResults/index.js
@@ -30,46 +30,25 @@ const SearchResults = () => {
     dispatch(search(searchKeyWord));
     setSearchKeyWord('')
     history.push(`/howto/${searchKeyWord}`);
-    // return <Redirect to={`/howto/${searchKeyWord}`} />;
   };
-
-
-
-  // click on cancel search button to clear search results
-
-  // let searchBlock;
-
-  // if (projects) {
-  //   //console.log(projects)
-  //   searchBlock = projects.map((project, i) => {
-  //     return (
-  //       <a key={`link${i}`} href={`/projects/${project.id}`}>
-  //         <div key={`liked'_${project.id}`}></div>
-  //       </a>
-  //     );
-  //   });
-  // }
 
   return (
     <>
-    <div>
-      <form title="Search Form" onSubmit={clickToSearch}>
-        <input
-          className="search-field"
-          type="text"
-          placeholder={errors}
-          value={searchKeyWord}
-          onChange={(e) => {
-            setSearchKeyWord(e.target.value);
-          }}
+      <div>
+        <form title="Search Form" onSubmit={clickToSearch}>
+          <input
+            className="search-field"
+            type="text"
+            placeholder={errors}
+            value={searchKeyWord}
+            onChange={(e) => {
+              setSearchKeyWord(e.target.value);
+            }}
           />
-        <button className='search-btn' type="submit">Search Button</button>
-      </form>
-    </div>
-      {/* {errors && errors.map(err => (
-        <p key={err}>{err}</p>
-        ))} */}
-        </>
+          <button className='search-btn' type="submit">Search Button</button>
+        </form>
+      </div>
+    </>
   );
 };
 
