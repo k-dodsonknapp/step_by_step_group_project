b78f612cd7667dbd95c08af3cdea9cc333b2cfba
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-12-06 20:35:04 -0800

    ASDF

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -19,24 +19,24 @@ seed_commands = AppGroup('seed')
 def seed():
     if environment == 'production':
     # Before seeding, truncate all tables prefixed with schema name
-        db.session.execute(f"TRUNCATE table {SCHEMA}.favorites RESTART IDENTITY CASCADE;")
-        db.session.execute(f"TRUNCATE table {SCHEMA}.views RESTART IDENTITY CASCADE;")
-        db.session.execute(f"TRUNCATE table {SCHEMA}.comments RESTART IDENTITY CASCADE;")
-        db.session.execute(f"TRUNCATE table {SCHEMA}.supplies RESTART IDENTITY CASCADE;")
-        db.session.execute(f"TRUNCATE table {SCHEMA}.instructions RESTART IDENTITY CASCADE;")
         db.session.execute(f"TRUNCATE table {SCHEMA}.project RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.favorites RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.comments RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.instructions RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.supplies RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.views RESTART IDENTITY CASCADE;")
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
         # Add other undo functions here
         # Add a truncate command here for every table that will be seeded.
         db.session.commit()
-    seed_users()
     # Add other seed functions here
-    seed_project()
-    seed_instructions()
-    seed_supplies()
-    seed_comments()
+    seed_users()
     seed_views()
+    seed_supplies()
+    seed_instructions()
+    seed_comments()
     seed_favorites()
+    seed_project()
 
 
 
